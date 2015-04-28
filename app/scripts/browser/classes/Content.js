f3d05084a39ce416d11a5283146db5ce3b8136e7
Oden <ops991923@gmail.com>
2015-04-28 13:22:26 -0700

    use nib, a bit of design on the content part

--- app/scripts/browser/classes/Content.js
+++ app/scripts/browser/classes/Content.js
@@ -11,7 +11,22 @@ export default class Content extends React.Component
 	{
 		return (
 			<main id="content">
-				<p>CONTENT</p>
+				<header>
+					<div id="info">
+						<img src="images/supermeatboy.png" />
+						<span>Super Meat Boy</span>
+					</div>
+				</header>
+
+				<footer>
+					<div id="share">
+						Share this game if you like it.
+					</div>
+
+					<div id="more-like">
+						More like this game:
+					</div>
+				</footer>
 			</main>
 		);
 	}
--- build/tasks/styles.js
+++ build/tasks/styles.js
@@ -3,14 +3,14 @@ var paths = require("../paths");
 var plumber = require("gulp-plumber");
 var stylus = require("gulp-stylus");
 var sourcemaps = require("gulp-sourcemaps");
-var axis = require("axis");
+var nib = require("nib");
 
 gulp.task("styles", function()
 {
 	return gulp.src(paths.styles)
 		.pipe(plumber())
 		.pipe(sourcemaps.init())
-		.pipe(stylus({use: [axis()], "include css": true}))
+		.pipe(stylus({use: [nib()], "include css": true}))
 		.pipe(sourcemaps.write())
 		.pipe(gulp.dest(paths.output + "styles"));
 });
