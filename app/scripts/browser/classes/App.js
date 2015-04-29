496124213df25494eabec5bbcd6b25c2b0e3e873
Oden <ops991923@gmail.com>
2015-04-28 17:22:43 -0700

    launching: added basic ability to launch and add games

--- app/scripts/browser/classes/App.js
+++ app/scripts/browser/classes/App.js
@@ -1,20 +1,41 @@
 import React from "react";
-import Sidebar from "./Sidebar";
-import Content from "./Content";
+import Game from "./Game";
 
 export default class App extends React.Component
 {
+	state = {
+		games: JSON.parse(localStorage.getItem("games") || "[]")
+	};
+
 	constructor(props)
 	{
 		super(props);
+	}
+
+	componentDidUpdate()
+	{
+		localStorage.setItem("games", JSON.stringify(this.state.games));
+	}
+
+	addGame()
+	{
+		let name = document.getElementById("name").value;
+		let path = document.getElementById("path").value;
+		this.setState({
+			games: this.state.games.concat({name: name, path: path})
+		});
 	}
 
 	render()
 	{
 		return (
 			<div>
-				<Sidebar />
-				<Content />
+				<input id="name" type="text" placeholder="Name" />
+				<input id="path" type="text" placeholder="Executable path" />
+				<button onClick={this.addGame.bind(this)}>Add game</button>
+
+				{this.state.games.map((game) =>
+					<Game key={game.name} name={game.name} path={game.path} />)}
 			</div>
 		);
 	}
