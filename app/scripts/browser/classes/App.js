f28f161211376902d14e3bc55740774928f0ae04
Philipp <philipp.arnosti@webtiser.ch>
2015-04-29 17:12:56 +0200

    Steam: load games from library

--- app/scripts/browser/classes/App.js
+++ app/scripts/browser/classes/App.js
@@ -14,20 +14,7 @@ export default class App extends React.Component
 			games.push(new Game(game.name, game.path));
 		}
 
-		let steamLocation = localStorage.getItem("steamLocation") || "/Users/chatz/Library/Application Support/Steam/SteamApps";
 
-		let steam = new Steam();
-		fs.readdir(steamLocation, function(err, files){
-			for (let file of files){
-				if (file.indexOf(".acf") != -1){
-					fs.readFile(steamLocation+"/"+file, {encoding:"utf-8"},function(err, data){
-						console.log(steam.parseAcf(data));
-
-					})
-					break;
-				};
-			}
-		});
 
 		this.state = {
 			open: null,
@@ -35,9 +22,32 @@ export default class App extends React.Component
 		};
 	}
 
+	updateSteamGames()
+	{
+		let steamLocation = localStorage.getItem("steamLocation");
+
+		let steam = new Steam();
+		let self = this;
+		fs.readdir(steamLocation, function(err, files){
+			for (let file of files){
+				if (file.indexOf(".acf") != -1){
+					fs.readFile(steamLocation+"/"+file, {encoding:"utf-8"},function(err, data){
+						let acfdata = steam.parseAcf(data)["AppState"];
+						self.setState({
+							games: self.state.games.concat(new Game(acfdata["name"],"steam://run/"+acfdata["appid"]))
+						});
+						console.log("new state",self.state);
+					})
+				};
+			}
+		});
+	}
+
 	componentDidUpdate()
 	{
 		localStorage.setItem("games", JSON.stringify(this.state.games));
+		localStorage.setItem("steamLocation",this.state.steamLocation);
+		this.updateSteamGames()
 	}
 
 	addGame()
@@ -49,6 +59,14 @@ export default class App extends React.Component
 		});
 		name.value = "";
 		path.value = "";
+	}
+
+	setSteamLocation()
+	{		
+		let steamLocation = document.getElementById("steamLocation");
+		this.setState({
+			steamLocation: steamLocation.value
+		});
 	}
 
 	launch(game)
@@ -63,6 +81,9 @@ export default class App extends React.Component
 		return (
 			<div>
 				<p>Currently open: {this.state.open || "None"}</p>
+				<input id="steamLocation" type="text" placeholder="steamLocation" />
+				<button onClick={this.setSteamLocation.bind(this)}>Set Steam Location</button>
+
 				<input id="name" type="text" placeholder="Name" />
 				<input id="path" type="text" placeholder="Executable path" />
 				<button onClick={this.addGame.bind(this)}>Add game</button>
